5a2f0926a13bf8cc9735bb5031b2400bc303f3cb
ArturHaavisto <arturhaavisto1@gmail.com>
2021-12-03 14:07:12 +0200

    something

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mysql = require("mysql");
+require("dotenv").config();
 const app = express();
 var cors = require("cors");
 app.use(cors());
@@ -9,6 +11,25 @@ const db = [{ name: "tiina" }, { name: "jack" }];
 
 app.get("/names", (req, res) => {
   res.send(db);
+});
+
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
+var pool = mysql.createPool(config);
+app.get("/", (req, res) => {
+  pool.query("SELECT * from locations", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
